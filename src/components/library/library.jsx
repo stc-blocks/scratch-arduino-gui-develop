c75864011a40914cd2ca92e4084bc282f032e836
RIjvan-Juneja <junejarijvan8128@gmail.com>
2025-06-30 00:05:30 +0530

    fixes backdrop image list

--- src/components/library/library.jsx
+++ src/components/library/library.jsx
@@ -226,7 +226,7 @@ class LibraryComponent extends React.Component {
                             featured={dataItem.featured}
                             helpLink={dataItem.helpLink}
                             hidden={dataItem.hidden}
-                            iconMd5={dataItem.costumes ? dataItem.costumes[0].md5ext : dataItem.md5ext}
+                            iconMd5={dataItem.costumes ? dataItem.costumes[0].md5ext : dataItem.md5}
                             iconRawURL={dataItem.rawURL}
                             icons={dataItem.costumes}
                             id={index}
